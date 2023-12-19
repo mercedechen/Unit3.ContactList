fc71c8623814f39e73cfbcb44db42595ae3ddac2
Mercede Chen <mercedechen@gmail.com>
2023-12-18 20:51:22 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h3>Contact List</h3>
+      <div>
+      </div>
+    </>
+  )
+}
+
+export default App
